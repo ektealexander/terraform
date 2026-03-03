acf7f4a3d581b02e57dfbf69a5fddca612a69389
ektealexander <alexander.spidsberg@gmail.com>
2026-03-03 11:50:35 +0100

    Update main.tf

--- ak1/main.tf
+++ ak1/main.tf
@@ -13,8 +13,102 @@ provider "azurerm" {
   subscription_id = "fedc4ad6-1398-4394-9fdc-fe5b7c031583"
 }
 
+
 #create a resource group
 resource "azurerm_resource_group" "terraform" {
   name     = "terraform"
   location = "Norway East"
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  address_space       = ["192.168.0.0/16"]
+  location            = azurerm_resource_group.terraform.location
+  resource_group_name = azurerm_resource_group.terraform.name
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.terraform.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["192.168.0.0/24"]
+}
+
+resource "azurerm_network_interface" "vm1" {
+  name                = "example-nic"
+  location            = azurerm_resource_group.terraform.location
+  resource_group_name = azurerm_resource_group.terraform.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_network_security_group" "ssh_to_vm2" {
+  name                = "allow_ssh_to_vm2"
+  location            = azurerm_resource_group.terraform.location
+  resource_group_name = azurerm_resource_group.terraform.name
+
+  security_rule {
+    name                       = "test123"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "22"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+
+resource "azurerm_linux_virtual_machine" "vm1" {
+  name                = "vm1"
+  resource_group_name = azurerm_resource_group.terraform.name
+  location            = azurerm_resource_group.terraform.location
+  size                = "Standard_F2"
+  admin_username      = "alespiadm"
+  admin_password	  = "Password123"
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm2" {
+  name                = "vm2"
+  resource_group_name = azurerm_resource_group.terraform.name
+  location            = azurerm_resource_group.terraform.location
+  size                = "Standard_F2"
+  admin_username      = "alespiadm"
+  admin_password	  = "Password123"
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
 }
